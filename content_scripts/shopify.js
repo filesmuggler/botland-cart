e4cc8899b7891fb0315e89ed306bc9d70c58655a
filesmuggler <krzste09@outlook.com>
2019-08-12 00:35:23 +0200

    gets basic data; needs formating

--- content_scripts/shopify.js
+++ content_scripts/shopify.js
@@ -1,4 +1,52 @@
 console.log("Shopify succesfully loaded!");
+
+/**
+ * Read content of the cart
+ */
+
+ (function(){
+
+  var tabela = document.getElementById("confirmCheckout").firstChild;
+
+  for(var i = 0, row; row = tabela.rows[i]; i++)
+  {
+      if(row.className=="checkout-single-item"){
+          
+          for (var j = 0, col; col = row.cells[j]; j++) {
+              //console.log("hello "+j);
+              
+              
+              
+              if(col.className=="supercheckout-name"){
+                // get name of the part
+                console.log(col.querySelectorAll("div a")[0].innerHTML);                    
+              }
+              if(col.className=="supercheckout-qty supercheckout-product-qty-input"){
+                // get number of parts
+                console.log(col.querySelectorAll("div input")[1].value);
+              }
+              if(col.className=="supercheckout-unit-total"){
+                // get unit price
+                console.log(col.querySelectorAll("span span")[0].innerHTML)
+              }
+              if(col.className=="supercheckout-total"){
+                // get total price
+                console.log(col.innerHTML)
+                
+                  
+              }
+              
+              
+              
+          } 
+      }
+    
+    
+}
+
+ })();
+
+
 (function() {
     /**
      * Check and set a global guard variable.
@@ -11,18 +59,22 @@ console.log("Shopify succesfully loaded!");
     window.hasRun = true;
   
     /**
-     * Given a URL to a beast image, remove all existing beasts, then
-     * create and style an IMG node pointing to
-     * that image, then insert the node into the document.
+     * Save table to CSV file.
      */
     function shopifyCsv() {
         console.log("csv");
     }
 
+    /**
+     * Save table to XLSX file.
+     */
     function shopifyXlsx() {
         console.log("xlsx");
     }
 
+    /**
+     * Share table as a list of elements.
+     */
     function shareCart() {
         console.log("share");
     }
